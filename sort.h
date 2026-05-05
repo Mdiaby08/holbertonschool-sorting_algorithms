fc58552e2b9d215b0bf685766452985507ff82f1
Mdiaby08 <diabymassire93@gmail.com>
2026-05-05 11:15:40 +0000

    formated sort.h

--- sort.h
+++ sort.h
@@ -36,7 +36,8 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-/* Deck sorting (1000-sort_deck.c)
+/**
+ * enum kind_e - Card suits
  * @SPADE: Spade suit
  * @HEART: Heart suit
  * @CLUB: Club suit
