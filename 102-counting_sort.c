7bf2191a74a36fbda01d0e42be784f5fc3d05def
Mdiaby08 <diabymassire93@gmail.com>
2026-05-05 11:29:09 +0000

    102-O, 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -24,6 +24,51 @@ static int get_max(int *array, size_t size)
 }
 
 /**
+ * build_count - Builds the cumulative count array
+ *
+ * @array: The original array
+ * @size: Number of elements in @array
+ * @count: The count array to fill
+ * @max: The maximum value in @array
+ */
+static void build_count(int *array, size_t size, int *count, int max)
+{
+	size_t i;
+	int j;
+
+	for (j = 0; j <= max; j++)
+		count[j] = 0;
+
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+
+	for (j = 1; j <= max; j++)
+		count[j] += count[j - 1];
+}
+
+/**
+ * rebuild_array - Reconstructs the sorted array from count and output
+ *
+ * @array: The original array to overwrite
+ * @size: Number of elements in @array
+ * @count: The cumulative count array
+ * @output: Temporary output buffer
+ */
+static void rebuild_array(int *array, size_t size, int *count, int *output)
+{
+	size_t i;
+
+	for (i = size; i > 0; i--)
+	{
+		output[count[array[i - 1]] - 1] = array[i - 1];
+		count[array[i - 1]]--;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+}
+
+/**
  * counting_sort - Sorts an array of integers in ascending order
  *                 using the Counting sort algorithm
  *
@@ -35,8 +80,6 @@ void counting_sort(int *array, size_t size)
 	int *count;
 	int *output;
 	int max;
-	size_t i;
-	int j;
 
 	if (!array || size < 2)
 		return;
@@ -54,25 +97,9 @@ void counting_sort(int *array, size_t size)
 		return;
 	}
 
-	for (j = 0; j <= max; j++)
-		count[j] = 0;
-
-	for (i = 0; i < size; i++)
-		count[array[i]]++;
-
-	for (j = 1; j <= max; j++)
-		count[j] += count[j - 1];
-
+	build_count(array, size, count, max);
 	print_array(count, max + 1);
-
-	for (i = size; i > 0; i--)
-	{
-		output[count[array[i - 1]] - 1] = array[i - 1];
-		count[array[i - 1]]--;
-	}
-
-	for (i = 0; i < size; i++)
-		array[i] = output[i];
+	rebuild_array(array, size, count, output);
 
 	free(count);
 	free(output);
