aaed18ed4e4df68081ab7e884b137c834e946ab9
Mdiaby08 <diabymassire93@gmail.com>
2026-05-05 10:05:57 +0000

    Add all files and haders file

---
+++ sort.h
@@ -0,0 +1,29 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/* Provided printing functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* Prototypes */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
